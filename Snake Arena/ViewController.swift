060dd3d62fffad0bbd3234be3f26b91a636b0029
arish khan <mohammadarishkhan503@gmail.com>
2022-04-07 19:35:27 +0530

    Initial Commit

---
+++ Snake Arena/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Snake Arena
+//
+//  Created by Arish Khan on 07/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
